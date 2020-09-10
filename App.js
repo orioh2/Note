e4b7f8c45c71faaf54bfc06a39d8580a4e1c7f18
Tizcom <45397970+Tizcom@users.noreply.github.com>
2020-09-10 13:47:08 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,29 @@
 import React from 'react';
-import { Picker, View, StyleSheet, Text } from 'react-native';
+import { Switch, Text, View, StyleSheet } from 'react-native';
 
 export default class App extends React.Component {
-  state = {choosenLabel: '', choosenindex: ''}
+  state = {switchValue:false}
+  toggleSwitch = (value) => {
+      this.setState({switchValue: value})
+   }
   render() {
     return (
       <View style={styles.container}>
-        <Text style = {styles.text}>{this.state.choosenLabel}</Text>
-        <Text style = {styles.text}>{this.state.choosenindex}</Text>
-        <Picker selectedValue={this.state.choosenLabel}
-          onValueChange={
-          (itemValue, itemIndex) => this.setState({
-               choosenLabel: itemValue, 
-               choosenindex:itemIndex})
-        }>
-            <Picker.Item label = "JavaScript" value = "No.1" />
-            <Picker.Item label = "TypeScript" value = "No.2" />
-            <Picker.Item label = "Angular" value = "No.3" />
-            <Picker.Item label = "React" value = "No.4" />
-            <Picker.Item label = "Vue" value = "No.5" />            
-        </Picker>        
+        <Text>{this.state.switchValue?
+          'Switch is ON':'Switch is OFF'}
+        </Text>
+        <Switch
+          style={{marginTop:30}}
+          onValueChange = {this.toggleSwitch}
+          value = {this.state.switchValue}/>           
       </View>
     );  
   } 
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    flexDirection: 'column'
-  },
-  text: {
-      fontSize: 20,
-      alignSelf: 'center',
-   }
+    alignItems: 'center',
+  }
 });
