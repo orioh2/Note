c458a8e0799d091c0006e4468c5357673ec1f30b
Tizcom <45397970+Tizcom@users.noreply.github.com>
2020-09-10 15:47:42 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,4 @@
-import React, { Component } from 'react';
-import Slider from '@react-native-community/slider'
-import { View, Text, StyleSheet } from 'react-native';
-
-export default class App extends Component {
-   constructor(props) {
-   super(props);
-   this.state = {
-     sliderValue: 50
-   };
- }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={{color: 'black'}}>
-          Value : {this.state.sliderValue}</Text>      
-        <Slider 
-          maximumValue={100}
-          minimumValue={0}
-          minimumTrackTintColor="#307ecc"
-          maximumTrackTintColor="#000000"
-          step={1} 
-          value={this.state.sliderValue}
-          onValueChange={(sliderValue) => 
-            this.setState({ sliderValue })}
-        />      
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   padding:20,
-   justifyContent: 'center',
-   backgroundColor: '#ecf0f1',
- }
-});
+npm install @react-navigation/native --save
+npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view --save
+npm install @react-navigation/material-top-tabs react-native-tab-view
+npm install @react-navigation/stack --save
