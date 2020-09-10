8f334ddbf7bc9fef32ba81ea1172db43b9e93153
Tizcom <45397970+Tizcom@users.noreply.github.com>
2020-09-10 15:35:35 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
-import { WebView } from "react-native-webview";
+import Slider from '@react-native-community/slider'
+import { View, Text, StyleSheet } from 'react-native';
 
 export default class App extends Component {
+   constructor(props) {
+   super(props);
+   this.state = {
+     sliderValue: 50
+   };
+ }
+
   render() {
     return (
-       <WebView
-        source={{uri: 'https://www.iverson.co.th/'}}
-        style={{marginTop: 20}}
-      />
+      <View style={styles.container}>
+        <Text style={{color: 'black'}}>
+          Value : {this.state.sliderValue}</Text>      
+        <Slider 
+          maximumValue={100}
+          minimumValue={0}
+          minimumTrackTintColor="#307ecc"
+          maximumTrackTintColor="#000000"
+          step={1} 
+          value={this.state.sliderValue}
+          onValueChange={(sliderValue) => 
+            this.setState({ sliderValue })}
+        />      
+      </View>
     );
   }
 }
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   padding:20,
+   justifyContent: 'center',
+   backgroundColor: '#ecf0f1',
+ }
+});
