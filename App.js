6c5afac08996631e1e15265f859229982b3c64a9
Tizcom <45397970+Tizcom@users.noreply.github.com>
2020-09-11 15:22:59 +0700

    Update App.js

--- App.js
+++ App.js
@@ -25,3 +25,24 @@ const styles = StyleSheet.create({
 });
 
 export default Mybutton;
+
+===============================================================================
+  
+import React from 'react';
+import { TouchableHighlight, Text, StyleSheet } from 'react-native';
+
+const Mytext = (props) => {
+  return <Text style={styles.text}>{props.text}</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#111825',
+    fontSize: 18,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+  },
+});
+
+export default Mytext;
