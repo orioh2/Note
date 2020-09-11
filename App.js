a45f25a17cfe47cf2e6cceabc5085b328566879e
Tizcom <45397970+Tizcom@users.noreply.github.com>
2020-09-11 11:13:21 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,92 @@
-react-native init CurrentLocation 
-Install : npm install @react-native-community/geolocation --save
-AndroidManifest.xml : <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+import React from 'react';
+import {View, Text,  StyleSheet, 
+  PermissionsAndroid,Platform} from 'react-native';
+import Geolocation from 
+'@react-native-community/geolocation';
+
+export default class App extends React.Component {
+  state = {
+    currentLongitude: 'unknown',//Initial Longitude
+    currentLatitude: 'unknown',//Initial Latitude
+ }
+ componentDidMount = () => {
+  var that =this;
+  if(Platform.OS === 'ios'){
+    this.callLocation(that);
+  }else{
+    async function requestLocationPermission() {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.
+          ACCESS_FINE_LOCATION,{
+            'title': 'Location Access Required',
+            'message': 'This App needs to Access your location'
+          }
+        )
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          that.callLocation(that);
+        } else {
+          alert("Permission Denied");
+        }
+      } catch (err) {
+        alert("err",err);
+        console.warn(err)
+      }
+    }
+    requestLocationPermission();
+  }    
+ }
+ callLocation(that){
+      Geolocation.getCurrentPosition(
+          (position) => {
+          const currentLongitude = 
+          JSON.stringify(position.coords.longitude);          
+          const currentLatitude = 
+          JSON.stringify(position.coords.latitude);
+          that.setState({ currentLongitude:currentLongitude });
+          that.setState({ currentLatitude:currentLatitude });          
+       },
+       (error) => alert(error.message),
+       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    that.watchID = Geolocation.watchPosition((position) => {
+        console.log(position);
+        const currentLongitude = JSON.stringify(position.coords.longitude);
+        const currentLatitude = JSON.stringify(position.coords.latitude);
+        that.setState({ currentLongitude:currentLongitude });
+        that.setState({ currentLatitude:currentLatitude });
+    });
+ }
+ componentWillUnmount = () => {
+    Geolocation.clearWatch(this.watchID);
+ }
+ render() {
+    return (
+       <View style = {styles.container}>
+          <Text style = {styles.boldText}>
+             ::: Location :::
+          </Text>
+          <Text style={{justifyContent:'center',alignItems: 'center',marginTop:16}}>
+            Longitude: {this.state.currentLongitude}
+          </Text>
+          <Text style={{justifyContent:'center',alignItems: 'center',marginTop:16}}>
+            Latitude: {this.state.currentLatitude}
+          </Text>
+       </View>
+    )
+ }
+}
+const styles = StyleSheet.create ({
+ container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center',
+    marginTop: 50,
+    padding:16,
+    backgroundColor:'white'
+ },
+ boldText: {
+    fontSize: 30,
+    color: 'red',
+ }
+})
